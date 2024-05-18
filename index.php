c6bf4f512d50667e5b48b20ca31ae736bf720013
maulanairvan <maulanairvann77@gmail.com>
2024-05-18 15:01:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>ISFA SHOP | Toko Baju Online</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>ISFA SHOP | Toko Baju Online</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>copyright by | kelompok 4 PTI C 2022 | Repost by <a href="" target="_blank">ISFA SHOP</a></p>
+  </div>
+
+</body>
+
+</html>
